711a46ae15076061231b35bd177956deea112a95
Sanket Tembhare <71713746+sanket010@users.noreply.github.com>
2023-10-03 23:17:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+
+import './App.css';
+import Navbar from './components/Navbar'
+import './components/container.css';
+import Containerone from './components/container1';
+import Containertwo from './components/container2';
+
+
+
+function App() {
+  return ( 
+    
+   <><><>
+      <>
+        <Navbar title="liner" /></>
+      <>
+        <Containerone title="head" /></>
+      <>
+        <Containertwo title="body" /></>
+    </> </></>
+   
+
+ 
+
+
+
+ );
+}
+
+export default App;
