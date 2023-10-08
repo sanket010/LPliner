579c830206ab333113b9d75a16149f3206d728ba
Sanket Tembhare <71713746+sanket010@users.noreply.github.com>
2023-10-08 23:37:49 +0530

    Add files via upload

--- container3.jsx
+++ container3.jsx
@@ -1,5 +1,10 @@
 import  React  from 'react';
 import './container.css';
+import samsung from './imgs/samsung.jpg';
+import forebs from './imgs/forbes.jpg';
+import tesla from './imgs/tesla.jpg';
+import slack from './imgs/slack.jpg';
+import ibm from './imgs/ibm.jpg';
 
 
 
@@ -10,7 +15,32 @@ import './container.css';
 const Containerthree = () => {
     return(
 
-        <div></div>
+        <div>
+          <h3>560.065+websites created by liner</h3>
+        
+        <div>
+          
+          <img className='img' src={samsung}  alt="" />
+        </div>
+        <div>
+          
+          <img className='img' src={ibm} alt="" />
+        </div>
+        <div>
+          
+          <img className='img' src={tesla} alt="" />
+        </div>
+        <div>
+          
+          <img className='img' src={slack} alt="" />
+        </div>
+        <div>
+        
+        <img className='img' src={forebs} alt="" />
+      </div>
+      
+    </div>
+   
 
 
 
